3d0422de4ba714926aa002242673fec3077ad789
James Thomas <james@jamesthom.as>
2018-02-05 16:19:21 +0000

    Swift 4 Codable support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,24 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OpenWhiskAction",
-    dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", "15.0.1")
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "OpenWhiskAction",
+            targets: ["OpenWhiskAction"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "OpenWhiskAction",
+            path: "Sources"),
+        .testTarget(
+            name: "OpenWhiskActionTests",
+            dependencies: ["OpenWhiskAction"]),
     ]
 )
--- Sources/epilogue.swift
+++ Sources/epilogue.swift
@@ -1,54 +1,94 @@
-
-/*
- * Copyright 2015-2016 IBM Corporation
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// Deliberate whitespaces above.
-
-/* This code is appended to user-supplied action code.
- It reads from the standard input, deserializes into JSON and invokes the
- main function. Currently, actions print strings to stdout. This can evolve once
- JSON serialization is available in Foundation. */
-
+// Imports
 import Foundation
 
-#if os(Linux)
-    import Glibc
-#endif
+let env = ProcessInfo.processInfo.environment
+let inputStr: String = readLine() ?? "{}"
+let json = inputStr.data(using: .utf8, allowLossyConversion: true)!
 
-func _whisk_json2dict(txt: String) -> [String:Any]? {
-    if let data = txt.data(using: String.Encoding.utf8, allowLossyConversion: true) {
-        return WhiskJsonUtils.jsonDataToDictionary(jsonData: data)
-    }
-    return nil
-}
-
-
+// snippet of code "injected" (wrapper code for invoking traditional main)
 func _run_main(mainFunction: ([String: Any]) -> [String: Any]) -> Void {
-    let env = ProcessInfo.processInfo.environment
-    let inputStr: String = env["WHISK_INPUT"] ?? "{}"
+    print("------------------------------------------------")
+    print("Using traditional style for invoking action...([String: Any]) -> [String: Any]")
     
-    if let parsed = _whisk_json2dict(txt: inputStr) {
-        let result = mainFunction(parsed)
-        
-        if let respString = WhiskJsonUtils.dictionaryToJsonString(jsonDict: result) {
-            print("\(respString)")
-        } else {
-            WhiskJsonUtils.logError("Error converting \(result) to JSON string")
+    let parsed = try! JSONSerialization.jsonObject(with: json, options: []) as! [String: Any]
+    let result = mainFunction(parsed)
+    if JSONSerialization.isValidJSONObject(result) {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: result, options: [])
+            if let jsonStr = String(data: jsonData, encoding: String.Encoding.utf8) {
+                print("\(jsonStr)")
+            } else {
+                print("Error serializing data to JSON, data conversion returns nil string")
+            }
+        } catch {
+            print(("\(error)"))
         }
     } else {
-        WhiskJsonUtils.logError("Error: couldn't parse JSON input.")
+        print("Error serializing JSON, data does not appear to be valid JSON")
+    }
+}
+
+// snippet of code "injected" (wrapper code for invoking codable main - async - vanilla)
+func _run_main<In: Codable, Out: Codable>(mainFunction: (In, (Out?, Error?) -> Void) -> Void) {
+    print("------------------------------------------------")
+    print("Using codable style for invoking action (async style - vanilla)...")
+    
+    do {
+        let input = try JSONDecoder().decode(In.self, from: json)
+        
+        let resultHandler = { (out: Out?, error: Error?) in
+            if let error = error {
+                print("Action handler callback returned an error:", error)
+                return
+            }
+            
+            guard let out = out else {
+                print("Action handler callback did not return response or error.")
+                return
+            }
+            
+            do {
+                let jsonData = try JSONEncoder().encode(out)
+                let jsonString = String(data: jsonData, encoding: .utf8)
+                print("\(jsonString!)")
+            } catch let error as EncodingError {
+                print("JSONEncoder failed to encode Codable type to JSON string:", error)
+                return
+            } catch {
+                print("Failed to execute action handler with error:", error)
+                return
+            }
+        }
+        
+        mainFunction(input, resultHandler)
+    } catch let error as DecodingError {
+        print("JSONDecoder failed to decode JSON string to Codable type:", error)
+        return
+    } catch {
+        print("Failed to execute action handler with error:", error)
+        return
+    }
+}
+
+// snippet of code "injected" (wrapper code for invoking codable main - sync - vanilla)
+func _run_main<In: Codable, Out: Codable>(mainFunction: (In) -> Out) -> Void {
+    print("------------------------------------------------")
+    print("Using codable style for invoking action (sync style - vanilla)...")
+    
+    do {
+        let input = try JSONDecoder().decode(In.self, from: json)
+        let out = mainFunction(input)
+        let jsonData = try JSONEncoder().encode(out)
+        let jsonString = String(data: jsonData, encoding: .utf8)
+        print("\(jsonString!)")
+    } catch let error as DecodingError {
+        print("JSONDecoder failed to decode JSON string to Codable type:", error)
+        return
+    } catch let error as EncodingError {
+        print("JSONEncoder failed to encode Codable type to JSON string:", error)
+        return
+    } catch {
+        print("Failed to execute action handler with error:", error)
+        return
     }
 }
