c635725ac84222a98dd9d6f52d692db9005c3218
James Thomas <james@jamesthom.as>
2018-03-27 17:13:01 +0100

    ...

--- Sources/OpenWhiskAction.swift
+++ Sources/OpenWhiskAction.swift
@@ -2,6 +2,6 @@ public func OpenWhiskAction(main: ([String: Any]) -> [String: Any]) -> Void {
   return _run_main(mainFunction: main) 
 }
 
-public func OpenWhiskAction<In: Codable, Out: Codable>(main: @escaping (In, (Out?, Error?) -> Void) -> Void) {
+public func OpenWhiskAction<In: Codable, Out: Codable>(main: (In, @escaping (Out?, Error?) -> Void) -> Void) {
     return _run_main(mainFunction: main)
 }
